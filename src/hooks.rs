fc3f4d05b4912da1f2d2a32a5e4c1648f6a75d19
kh4f <kh4f.dev@gmail.com>
2026-03-07 19:31:29 +0300

    refactor(hooks): simplify event handling for keyboard and mouse
    Consolidate event handling logic into `handle_keyboard_event` and `handle_mouse_event` functions.

--- src/hooks.rs
+++ src/hooks.rs
@@ -1,37 +1,22 @@
 use std::process::Command;
 use windows:: Win32::{ Foundation::*, UI:: WindowsAndMessaging::* };
-use crate::{ CONFIG, config::MouseEvent, utils::{ open_or_focus_app, press_keys } };
+use crate::{ CONFIG, config::{ Action, MouseEvent }, utils::{ open_or_focus_app, press_keys } };
 
 pub unsafe extern "system" fn keyboard_proc(n_code: i32, w_param: WPARAM, l_param: LPARAM) -> LRESULT {
     if n_code >= 0 {
         let info = unsafe { &*(l_param.0 as *const KBDLLHOOKSTRUCT) };
-		let vk_code = info.vkCode;
-
-		let mut pt = POINT::default();
+        let mut pt = POINT::default();
         unsafe { GetCursorPos(&mut pt).ok(); };
 
-		if let Some(cfg) = CONFIG.get() {
-			for rule in &cfg.rules {
-				if rule.enabled
-					&& let Some(key) = rule.trigger.key
-					&& key.0 as u32 == vk_code
-					&& rule.trigger.region.contains(pt)
-				{
-					if let Some(keys) = &rule.action.keys { press_keys(keys); }
-					if let Some(cmd) = &rule.action.cmd { Command::new(cmd).spawn().ok(); }
-					if let Some(open) = &rule.action.open { open_or_focus_app(open); }
-					if rule.consume.unwrap_or(false) { return LRESULT(1) }
-				}
-			}
-		}
+        if handle_keyboard_event(info.vkCode, pt) { return LRESULT(1) }
     }
     unsafe { CallNextHookEx(None, n_code, w_param, l_param) }
 }
 
 pub unsafe extern "system" fn mouse_proc(n_code: i32, w_param: WPARAM, l_param: LPARAM) -> LRESULT {
-    if n_code >= 0 {
-        let info = unsafe { &*(l_param.0 as *const MSLLHOOKSTRUCT) };
-        let pt = info.pt;
+	if n_code >= 0 {
+		let info = unsafe { &*(l_param.0 as *const MSLLHOOKSTRUCT) };
+		let pt = info.pt;
 
 		let mouse_event = match w_param.0 as u32 {
 			WM_LBUTTONDOWN => Some(MouseEvent::LeftDown),
@@ -45,20 +30,45 @@ pub unsafe extern "system" fn mouse_proc(n_code: i32, w_param: WPARAM, l_param: 
 			_ => None
 		};
 
-		if let Some(cfg) = CONFIG.get() {
-			for rule in &cfg.rules {
-				if rule.enabled
-					&& let Some(event) = mouse_event
-					&& rule.trigger.mouse.as_ref() == Some(&event)
-					&& rule.trigger.region.contains(pt)
-				{
-					if let Some(keys) = &rule.action.keys { press_keys(keys); }
-					if let Some(cmd) = &rule.action.cmd { Command::new(cmd).spawn().ok(); }
-					if let Some(open) = &rule.action.open { open_or_focus_app(open); }
-					if rule.consume.unwrap_or(false) { return LRESULT(1) }
-				}
-			}
-		}
+		if let Some(event) = mouse_event && handle_mouse_event(event, pt) { return LRESULT(1) }
+	}
+	unsafe { CallNextHookEx(None, n_code, w_param, l_param) }
+}
+
+fn handle_mouse_event(mouse_event: MouseEvent, pt: POINT) -> bool {
+    if let Some(cfg) = CONFIG.get() {
+        for rule in &cfg.rules {
+            if rule.enabled
+                && let Some(event) = rule.trigger.mouse
+                && event == mouse_event
+                && rule.trigger.region.contains(pt)
+            {
+                execute_action(&rule.action);
+                return rule.consume.unwrap_or(false)
+            }
+        }
     }
-    unsafe { CallNextHookEx(None, n_code, w_param, l_param) }
+    false
+}
+
+fn handle_keyboard_event(vk_code: u32, pt: POINT) -> bool {
+    if let Some(cfg) = CONFIG.get() {
+        for rule in &cfg.rules {
+            if rule.enabled
+                && let Some(key) = rule.trigger.key
+                && key.0 as u32 == vk_code
+                && rule.trigger.region.contains(pt)
+            {
+                execute_action(&rule.action);
+                return rule.consume.unwrap_or(false)
+            }
+        }
+    }
+    false
+}
+
+fn execute_action(action: &Action) {
+    if let Some(keys) = &action.keys { press_keys(keys); }
+    if let Some(cmd) = &action.cmd { Command::new(cmd).spawn().ok(); }
+    if let Some(open) = &action.open { open_or_focus_app(open); }
 }
