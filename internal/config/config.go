71488166edf594803c58ca4084384e5989cce22a
kh4f <kh4f.dev@gmail.com>
2026-03-19 20:02:48 +0300

    fix(config): avoid mutating shared default config
    Default config is now cloned on each request, avoiding side effects from shared references.

--- internal/config/config.go
+++ internal/config/config.go
@@ -5,13 +5,21 @@ var Cfg Config
 var defCfg = Config{
 	Rules: []Rule{
 		{
-			Name:    "Default Rule",
+			Name:    "Caps Lock to F13",
 			Enabled: true,
 			Trigger: &Trigger{Kb: ptr("VK_CAPITAL")},
 			Action:  &Action{Kb: []string{"VK_F13"}},
 			Consume: ptr(true),
 		},
 	},
+}
+
+func defaultConfig() Config {
+	cfg := Config{
+		Rules: make([]Rule, len(defCfg.Rules)),
+	}
+	copy(cfg.Rules, defCfg.Rules)
+	return cfg
 }
 
 func ptr[T any](v T) *T {
