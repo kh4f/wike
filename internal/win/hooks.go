177e4f4ad09b36c3f14313bf901dbea0cf24600b
kh4f <kh4f.dev@gmail.com>
2026-03-19 20:59:24 +0300

    refactor(hooks): improve mouse and keyboard event handling logic
    - Simplify condition checks for injected events in `mHook` and `kHook` functions.
    - Streamline rule matching and action execution for mouse and keyboard events.
    - Enhance readability and maintainability of event handling code.

--- internal/win/hooks.go
+++ internal/win/hooks.go
@@ -31,44 +31,44 @@ type KBDLLHOOKSTRUCT struct {
 }
 
 func mHook(nCode int, wParam uintptr, lParam uintptr) uintptr {
-	if nCode >= 0 {
-		info := (*MSLLHOOKSTRUCT)(unsafe.Pointer(lParam))
-		if (info.Flags & LLKHF_INJECTED) != 0 {
-			return callNextHook(nCode, wParam, lParam)
+	if nCode < 0 {
+		return callNextHook(nCode, wParam, lParam)
+	}
+
+	info := (*MSLLHOOKSTRUCT)(unsafe.Pointer(lParam))
+	if (info.Flags & LLKHF_INJECTED) != 0 {
+		return callNextHook(nCode, wParam, lParam)
+	}
+
+	mouseEvent := config.ParseMouseEvent(wParam, info.MouseData)
+	if mouseEvent.State != config.StateMove {
+		fmt.Printf("Mouse event: %+v (wParam=0x%X; mouseData=0x%X; pt=%d:%d)\n", mouseEvent, wParam, info.MouseData, info.Pt.X, info.Pt.Y)
+	}
+
+	for _, rule := range config.Cfg.Rules {
+		if !rule.Enabled || (rule.Region != nil && !rule.Region.Contains(info.Pt)) {
+			continue
 		}
 
-		x, y := info.Pt.X, info.Pt.Y
-		mouseEvent := config.ParseMouseEvent(wParam, info.MouseData)
-
-		if mouseEvent.State != config.StateMove {
-			fmt.Printf("Mouse event: %+v (wParam=0x%X; mouseData=0x%X; pt=%d:%d)\n", mouseEvent, wParam, info.MouseData, x, y)
-		}
-
-		for _, rule := range config.Cfg.Rules {
-			if !rule.Enabled || rule.Region != nil && !rule.Region.Contains(info.Pt) {
+		matched := false
+		for _, binding := range rule.BindingsWithPrimary() {
+			if binding.Trigger == nil ||
+				binding.Action == nil ||
+				binding.Trigger.M == nil ||
+				*binding.Trigger.M != mouseEvent.Btn {
 				continue
 			}
 
-			isRuleMatched := false
-
-			for _, b := range rule.BindingsWithPrimary() {
-				if b.Trigger == nil || b.Action == nil || b.Trigger.M == nil || *b.Trigger.M != mouseEvent.Btn {
-					continue
-				}
-
-				isRuleMatched = true
-
-				if b.Trigger.State != nil && *b.Trigger.State == mouseEvent.State ||
-					b.Trigger.State == nil && mouseEvent.State == config.StateDown {
-					fmt.Printf("Rule triggered: '%s'\n", rule.Name)
-					executeAction(*b.Action)
-				}
+			matched = true
+			if shouldExecuteBinding(binding.Trigger.State, mouseEvent.State) {
+				fmt.Printf("Rule triggered: '%s'\n", rule.Name)
+				executeAction(*binding.Action)
 			}
+		}
 
-			if isRuleMatched && rule.Consume != nil && *rule.Consume {
-				fmt.Println("Event consumed")
-				return 1
-			}
+		if matched && rule.Consume != nil && *rule.Consume {
+			fmt.Println("Event consumed")
+			return 1
 		}
 	}
 
@@ -76,47 +76,56 @@ func mHook(nCode int, wParam uintptr, lParam uintptr) uintptr {
 }
 
 func kHook(nCode int, wParam uintptr, lParam uintptr) uintptr {
-	if nCode >= 0 {
-		info := (*KBDLLHOOKSTRUCT)(unsafe.Pointer(lParam))
-		if (info.Flags & LLKHF_INJECTED) != 0 {
-			return callNextHook(nCode, wParam, lParam)
+	if nCode < 0 {
+		return callNextHook(nCode, wParam, lParam)
+	}
+
+	info := (*KBDLLHOOKSTRUCT)(unsafe.Pointer(lParam))
+	if (info.Flags & LLKHF_INJECTED) != 0 {
+		return callNextHook(nCode, wParam, lParam)
+	}
+
+	var pt shared.Point
+	getCursorPos(uintptr(unsafe.Pointer(&pt)))
+	kbEvent := config.ParseKbEvent(uint16(info.VkCode), info.Flags)
+
+	fmt.Printf("Kb event: %+v (wParam=0x%X; vkCode=0x%X; pt=%d:%d)\n", kbEvent, wParam, info.VkCode, pt.X, pt.Y)
+
+	for _, rule := range config.Cfg.Rules {
+		if !rule.Enabled || (rule.Region != nil && !rule.Region.Contains(pt)) {
+			continue
 		}
 
-		var pt shared.Point
-		getCursorPos(uintptr(unsafe.Pointer(&pt)))
-		kbEvent := config.ParseKbEvent(uint16(info.VkCode), info.Flags)
-
-		fmt.Printf("Key event: %+v (wParam=0x%X; VkCode=0x%X; pt=%d:%d)\n", kbEvent, wParam, info.VkCode, pt.X, pt.Y)
-
-		for _, rule := range config.Cfg.Rules {
-			if !rule.Enabled {
+		matched := false
+		for _, binding := range rule.BindingsWithPrimary() {
+			if binding.Trigger == nil ||
+				binding.Action == nil ||
+				binding.Trigger.Kb == nil ||
+				*binding.Trigger.Kb != kbEvent.Key {
 				continue
 			}
 
-			shouldConsume := false
-
-			for _, b := range rule.BindingsWithPrimary() {
-				if b.Trigger == nil || b.Action == nil || b.Trigger.Kb == nil || *b.Trigger.Kb != kbEvent.Key {
-					continue
-				}
-
-				shouldConsume = true
-
-				if b.Trigger.State != nil && *b.Trigger.State == kbEvent.Event ||
-					b.Trigger.State == nil && kbEvent.Event == config.StateDown {
-					fmt.Printf("Rule triggered: '%s'\n", rule.Name)
-					executeAction(*b.Action)
-				}
+			matched = true
+			if shouldExecuteBinding(binding.Trigger.State, kbEvent.Event) {
+				fmt.Printf("Rule triggered: '%s'\n", rule.Name)
+				executeAction(*binding.Action)
 			}
+		}
 
-			if shouldConsume && rule.Consume != nil && *rule.Consume {
-				fmt.Println("Event consumed")
-				return 1
-			}
+		if matched && rule.Consume != nil && *rule.Consume {
+			fmt.Println("Event consumed")
+			return 1
 		}
 	}
 
 	return callNextHook(nCode, wParam, lParam)
+}
+
+func shouldExecuteBinding(triggerState *config.State, eventState config.State) bool {
+	if triggerState == nil {
+		return eventState == config.StateDown
+	}
+	return *triggerState == eventState
 }
 
 func callNextHook(nCode int, wParam uintptr, lParam uintptr) uintptr {
