8ea5fa2a89226993ea436fca2ea6f34ec0f5aa6f
kh4f <kh4f.dev@gmail.com>
2026-03-19 17:10:50 +0300

    refactor(vkcodes): simplify VKCodeMap initialization
    Replace constant definitions with direct values in the `VKCodeMap` for improved readability and maintainability.

--- internal/config/vkcodes.go
+++ internal/config/vkcodes.go
@@ -1,17 +1,10 @@
 package config
 
-const (
-	VK_CAPITAL     = 0x14
-	VK_F13         = 0x7C
-	VK_VOLUME_DOWN = 0xAE
-	VK_VOLUME_UP   = 0xAF
-)
-
 var VKCodeMap = map[string]uint16{
-	"VK_F13":         VK_F13,
-	"VK_CAPITAL":     VK_CAPITAL,
-	"VK_VOLUME_UP":   VK_VOLUME_UP,
-	"VK_VOLUME_DOWN": VK_VOLUME_DOWN,
+	"VK_F13":         0x7C,
+	"VK_CAPITAL":     0x14,
+	"VK_VOLUME_UP":   0xAF,
+	"VK_VOLUME_DOWN": 0xAE,
 }
 
 var RevVKCodeMap = func() map[uint16]string {
