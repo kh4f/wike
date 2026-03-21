0678dec8345d07dcc647d3e566dad0912ede210f
kh4f <kh4f.dev@gmail.com>
2026-03-22 00:47:44 +0300

    refactor(app): move initialization logic from `app.go` to `main.go`
    Relocates application startup and configuration logic directly into `main.go` for improved clarity and maintainability.

--- cmd/wike/main.go
+++ cmd/wike/main.go
@@ -1,9 +1,27 @@
-package main
+package app
 
 import (
-	"wike/internal/app"
+	"time"
+	"wike/internal/config"
+	"wike/internal/win"
 )
 
 func main() {
-	app.Run()
+	win.InitScreenSize()
+	config.Cfg.Load()
+
+	win.InstallHooks()
+
+	go reloadConfigLoop()
+
+	win.RunMessageLoop()
+}
+
+func reloadConfigLoop() {
+	ticker := time.NewTicker(5 * time.Second)
+	defer ticker.Stop()
+
+	for range ticker.C {
+		config.Cfg.ReloadIfModified()
+	}
 }
