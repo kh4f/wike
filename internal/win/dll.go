224938d29af096cd6290edb95a1014938fbe435b
kh4f <kh4f.dev@gmail.com>
2026-03-24 17:14:53 +0300

    style(dll): align variable declarations in `dll.go`

--- internal/win/dll.go
+++ internal/win/dll.go
@@ -3,7 +3,7 @@ package win
 import "golang.org/x/sys/windows"
 
 var (
-	user32  = windows.NewLazySystemDLL("user32.dll")
-	shell32 = windows.NewLazySystemDLL("shell32.dll")
+	user32   = windows.NewLazySystemDLL("user32.dll")
+	shell32  = windows.NewLazySystemDLL("shell32.dll")
 	kernel32 = windows.NewLazySystemDLL("kernel32.dll")
 )
